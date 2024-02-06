505c3f5580c61f2aff222df54b43e78e2340de01
Luis Nava <jorgex4p@gmail.com>
2024-02-06 10:36:05 -0500

    v3.5

---
+++ src/components/ButtonLoadMore/ButtonLoadMore.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    width: 200px;
+    height: 50px;
+
+    background-color: #8a2ec3ba;
+    border: none;
+    border-radius: 5px;
+    color: white;
+` 
